d998123b205bdf1762edaeb05bf7b3c747518cc3
chenzhichao <80139283@qq.com>
2025-08-29 08:08:34 +0800

    add

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+
+// rcli csv -i input.csv -o output.json  --header --d ',' 
+
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(name = "rcli", version, author, about, long_about = None)]
+struct Options {
+
+    #[command(subcommand)]
+    cmd: SubCommand,
+}
+
+
+#[derive(Debug,Subcommand)]
+enum SubCommand {
+
+    #[command(name = "csv", about = "Show CSV, or convert CSV to other formats")]
+    Csv(CsvOpts),
+}
+
+#[derive(Parser, Debug)]
+struct CsvOpts {
+
+    #[arg(short, long, help = "Input file path")]
+    input: String,
+
+    #[arg(short, long, help = "Output file path", default_value = "output.json")]
+    output: String,
+
+    #[arg(short, long, help = "Delimiter")]
+    delimiter: char,
+
+    #[arg(short, long, help = "CSV has header or not", default_value_t = true)]
+    header: bool,
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, rust!");
 }
